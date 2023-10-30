9251a96850c374cb32d048b66f7ce0ab2ab62b1f
Nathan <nate.campbell@hotmail.com>
2023-10-30 14:50:07 +1000

    Added screens and Const for screen size

--- sketch.js
+++ sketch.js
@@ -1,25 +1,48 @@
+"use strict";
+
+////CONSTANTS FOR THE SCREEN SIZE/////
+const playAreaX = 960;
+const playAreaY = 960;
+
+////SCREEN NUMBERS TO ALLOW MOVEMENT BETWEEN THEM/////
+////Editing note, make the screen you want to see 0 if you want to view it first/////
+let SCREENS = {
+  CURRENT : 0,
+  LANDING : 2,
+  MAIN : 1,
+  GAME : 0,
+  SCORE : 3,
+}
+
+
+function preload(){
+  
+}
+
+
 function setup() {
-  new Canvas(800, 400);
+  new Canvas(playAreaX, playAreaY);
 }
 
 function draw() {
-  background("red");
 
-  fill(0);
-  textSize(24);
-  textAlign(CENTER);
-  text('Click to create a new sprite', width / 2, height / 2);
-  text('Git Attemp 1', width / 2, height / 1.5);
+  if(SCREENS.CURRENT==SCREENS.GAME){
+    background("red");
+    fill(0);
+    textSize(24);
+    textAlign(CENTER);
+    text('Click to create a new sprite', width / 2, height / 2);
+    text('Git Attemp 1', width / 2, height / 1.5);
 
-  if (mouse.presses()) {
+    if (mouse.presses()) {
 
-    let sprite = new Sprite(mouse.x, mouse.y, 30, 30);
+      let sprite = new Sprite(mouse.x, mouse.y, 30, 30);
 
-    sprite.color = "yellow"
-    sprite.vel.x = random(-1, 1);
-    sprite.vel.y = random(-1, 1);
+      sprite.color = "yellow"
+      sprite.vel.x = random(-1, 1);
+      sprite.vel.y = random(-1, 1);
 
+    }
   }
-
 }
 
