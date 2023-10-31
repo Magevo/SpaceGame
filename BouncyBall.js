36a40300b3eeeb713187e8a4aa75185910970990
Nathan <nate.campbell@hotmail.com>
2023-11-01 09:04:57 +1000

    submited mostly complete factory class in bouncyball

--- BouncyBall.js
+++ BouncyBall.js
@@ -2,6 +2,107 @@ const W = 800
 const H = 400
 
 
+////This class remains idle until an object aka sprite is requested.
+/////When the object is requested, done by calling any function below createObject,
+/////it should create a sprite with the relevant image and resources.
+
+/////STILL TRYING TO FIGURE OUT HOW TO CALL STUFF OUT OF THE FACTORY EFFICIENTLY//////
+class objectCreation {
+  constructor(){
+    this.images = new Array(); ////Array for storing images.
+  }
+
+  preload(){
+    this.images.push({id: "resourceOne", path: "data/resources/Meteor_01.png"})
+    this.images.push({id: "resourceTwo", path: "data/resources/Meteor_02.png"})
+    this.images.push({id: "playerBase", path: "data/base/player_base.png"})
+    this.images.push({id: "enemyBase", path: "data/base/enemy_base.png"})
+    this.images.push({id: "playerShips", path: "data/combatShips/playerShips/Ship_LVL_1.png"})
+    this.images.push({id: "enemyShips", path: "data/combatShips/enemyShips/Ship_LVL_1.png"})
+    this.images.push({id: "shotRound", path: "data/shots/Rocket_Effect_01.png"})
+
+    for(let img of this.images){
+        img.image = loadImage(image.path);
+    }
+  }
+
+  ////This will loop through the array and return the requested image when run in functions below.
+  getImageByID(id){
+      for(let img of this.images){
+        if (img.id === id) {
+          return img.image;
+        }
+      }
+      return null;
+  }
+
+  createObject(x, y){
+    let obj = new Sprite(x, y);
+    return obj;
+  }
+
+  createResourceOne(x, y){
+    let obj = this.createObject(x, y);
+    obj.image = this.getImageByID("resourceOne");
+    obj.resource = 5;
+    obj.textSize = 20;
+    obj.text = obj.resource;
+    return obj;
+  }
+
+  createResourceTwo(x, y){
+    let obj = this.createObject(x, y);
+    obj.image = this.getImageByID("resourceTwo");
+    obj.resource = 5;
+    obj.textSize = 20;
+    obj.text = obj.resource;
+    return obj;
+  }
+
+  createPlayerBase(x, y){
+    let obj = this.createObject(x, y);
+    obj.image = this.getImageByID("playerBase");
+    obj.hp = 20;
+    obj.textSize = 20;
+    obj.text = obj.hp;
+    return obj;
+  }
+
+  createEnemyBase(x, y){
+    let obj = this.createObject(x, y);
+    obj.image = this.getImageByID("enemyBase");
+    obj.hp = 20;
+    obj.textSize = 20;
+    obj.text = obj.hp;
+    return obj;
+  }
+
+  createPlayerShips(x, y){
+    let obj = this.createObject(x, y);
+    obj.image = this.getImageByID("playerShips");
+    obj.hp = 4;
+    obj.textSize = 10;
+    obj.text = obj.hp;
+    return obj;
+  }
+
+  createEnemyShips(x, y){
+    let obj = this.createObject(x, y);
+    obj.image = this.getImageByID("enemyShips");
+    obj.hp = 4;
+    obj.textSize = 10;
+    obj.text = obj.hp;
+    return obj;
+  }
+
+  createShotRound(x, y){
+    let obj = this.createObject(x, y);
+    obj.image = this.getImageByID("shotRound");
+    return obj;
+  }
+}
+
+
 ////CLASS FOR CREATING BASES/////
 class Base {
   constructor(x, y){
